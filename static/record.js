938310589a82ccafca5089a4626a0d54fcccbfb2
Hanwei Tang <91324446+thw19990210@users.noreply.github.com>
2022-04-20 14:15:14 -0400

    Update record.js

--- static/record.js
+++ static/record.js
@@ -1,13 +1,14 @@
 function display_record(activity){
     $("#activity_record").empty();
+    let s = $("<tr><td>Time</td><td>Activity</td></tr>")
+    $("#activity_record").append(s);
     for (let key in activity){
-        let one= $("<div>"+key+"    "+activity[key]+"</div>")
+        let one = $("<tr><td>"+key+ "</td><td>"+activity[key]+"</td></tr>")
         $("#activity_record").append(one);
-        $("#activity_record").append("<br>");
+        // $("#activity_record").append("<br>");
     }
 
 }
-
 $(document).ready(function(){
     display_record(activity)
 })
