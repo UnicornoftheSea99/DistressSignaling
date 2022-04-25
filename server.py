79a954981849ceda3a5aa576cf997540c62e8c10
UnicornoftheSea99 <erhyu@smith.edu>
2022-04-25 05:52:17 -0400

    changing routes slightly so that signal fire activity is within wilderness section

--- server.py
+++ server.py
@@ -102,7 +102,7 @@ learning_data = {
     "10": { 
     "id": "10", 
     "page_title": "Signal Fires",
-    "info1": ["Why light a signal fire", "A signal fire is useful because it can be see for miles."],
+    "info1": ["Why light a signal fire?", "A signal fire is useful because it can be see for miles."],
     "info2": [ "What should a signal fire look like?",["One or Three Signal Fires in a Triangle Formation in an open space","(Depending on Resources) First layer dry tinder, second layer wood kindling, third layer moss and decaying plant life (optional), final layer green leafy vegetation"]],
     "info3":[ "",""],
     "media": ["https://www.youtube.com/embed/0G857JwMsM8"],
@@ -111,19 +111,24 @@ learning_data = {
     },
     "11": {
         "id": "11",
+        "category": "wilderness",
+        "subcategory": "signal_fire_activity"
+    },
+    "12": {
+        "id": "12",
         "page_title": "Wilderness Distress Signals Mini Quiz",
         "question": "Which of the following cannot be used as a signal mirror?",
         "a": "Belt Buckle",
         "b": "Mirror",
         "c": "Rock",
-        "d": "Credit Carf",
+        "d": "Credit Card",
         "answer": "c",
         "feedback": "All of the other options are reflective in some way and therefore can be used as a signal mirror to communicate distress.",
         "category": "wilderness",
         "subcategory": "quiz",
     },      
-     "12": { 
-    "id": "12", 
+     "13": { 
+    "id": "13", 
     "page_title": "Street Distress Signals",
     "info1": ["", "Aside from being lost at sea or stranded on a deserted island, you may have other reasons to signal for distress. And though most people seem to have easy access to a phone at all times, there are scenarios when calling for help may not be possible."],
     "info2": [ "In what specific scenarios may one need to call for help but be unable to?","Domestic Violence, Human Trafficking, and Kidnapping"],
@@ -132,8 +137,8 @@ learning_data = {
     "category": "streets",
     "subcategory": "intro"
     },
-     "13": { 
-    "id": "13", 
+     "14": { 
+    "id": "14", 
     "page_title": "Street Distress Signals",
     "info1": ["", ""],
     "info2": [ "",""],
@@ -142,8 +147,8 @@ learning_data = {
     "category": "streets",
     "subcategory": "video"
     },
-    "14": {
-        "id": "14",
+    "15": {
+        "id": "15",
         "page_title": "Street Distress Signals Mini Quiz",
         "question": "Which of the following is not a reason to use the international hand signal for help?",
         "a": "Domestic violence",
@@ -264,10 +269,12 @@ def learn_intro():
 @app.route('/learn/<category>/<subcategory>')
 def learn(category,subcategory):
     global activity
+    global fire_simulation
+    global learning_data
+
     time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     activity[time_str] = category+"/"+subcategory
-    
-    global learning_data
+
     page_info = {}
     for item in learning_data:
         # print(learning_data[item])
@@ -287,16 +294,18 @@ def learn(category,subcategory):
         return render_template('learningvideo.html', page_info = page_info, learning_data = learning_data,next_category=next_category,next_subcategory=next_subcategory,video_link=video_link)
     elif page_info["subcategory"] == "quiz":
         return render_template('miniquiz.html',page_info=page_info, learning_data=learning_data, next_category=next_category, next_subcategory=next_subcategory)
+    elif page_info["subcategory"] == "signal_fire_activity":
+        return render_template('firesim.html', fire_material = fire_simulation,learning_data=learning_data, next_category=next_category, next_subcategory=next_subcategory) 
     else:  
         return render_template('learning.html', page_info = page_info, learning_data = learning_data,next_category=next_category,next_subcategory=next_subcategory)
 
-@app.route('/learn/signal_fire_activity')
-def build_a_fire():
-    global fire_simulation
-    global activity
-    time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    activity[time_str] = 'signal fire activity'
-    return render_template('firesim.html', fire_material = fire_simulation)   
+# @app.route('/learn/signal_fire_activity')
+# def build_a_fire():
+#     global fire_simulation
+#     global activity
+#     time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+#     activity[time_str] = 'signal fire activity'
+#     return render_template('firesim.html', fire_material = fire_simulation)   
 
 # @app.route('/view/<id>')
 # def film_info(id = None):
